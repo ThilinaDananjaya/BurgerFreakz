9eec59f023baff107f7cb56695ffbbee7ed3150f
Thilina Dananjaya <66713156+ThilinaDananjaya@users.noreply.github.com>
2020-07-24 22:08:01 +0530

    Update signup_inc.php

--- includes/signup_inc.php
+++ includes/signup_inc.php
@@ -8,25 +8,25 @@ if (isset($_POST['signup_submit'])) {
     $password_repeat = $_POST['password_repeat'];
 
     if (empty($username) || empty($email) || empty($password) || empty($password_repeat)) {
-        header("Location: ../signup.php?error=emptyfields&username=" . $username . "&email=" . $email);
+        header("Location: ../signup.html?error=emptyfields&username=" . $username . "&email=" . $email);
         exit();
     } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-        header("Location: ../signup.php?error=invalidemailusername");
+        header("Location: ../signup.html?error=invalidemailusername");
         exit();
     } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: ../signup.php?error=invalidemail&username=" . $username);
+        header("Location: ../signup.html?error=invalidemail&username=" . $username);
         exit();
     } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-        header("Location: ../signup.php?error=invalidusername&email=" . $email);
+        header("Location: ../signup.html?error=invalidusername&email=" . $email);
         exit();
     } elseif ($password !== $password_repeat) {
-        header("Location: ../signup.php?error=passwordcheck&username=" . $username . "&email=" . $email);
+        header("Location: ../signup.html?error=passwordcheck&username=" . $username . "&email=" . $email);
         exit();
     } else {
         $sql = "SELECT user_username FROM users WHERE user_username=?";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
-            header("Location: ../signup.php?error=sqlerror");
+            header("Location: ../signup.html?error=sqlerror");
             exit();
         } else {
             mysqli_stmt_bind_param($stmt, "s", $username);
@@ -34,19 +34,19 @@ if (isset($_POST['signup_submit'])) {
             mysqli_stmt_store_result($stmt);
             $resultCheck = mysqli_stmt_num_rows($stmt);
             if ($resultCheck > 0) {
-                header("Location: ../signup.php?error=usertaken&email=" . $email);
+                header("Location: ../signup.html?error=usertaken&email=" . $email);
                 exit();
             } else {
                 $sql = "INSERT INTO users (user_username, user_email, user_password) VALUES (?, ?, ?)";
                 $stmt = mysqli_stmt_init($conn);
                 if (!mysqli_stmt_prepare($stmt, $sql)) {
-                    header("Location: ../signup.php?error=sqlerror");
+                    header("Location: ../signup.html?error=sqlerror");
                     exit();
                 } else {
                     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
                     mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashed_password);
                     mysqli_stmt_execute($stmt);
-                    header("Location: ../signup.php?signup=success");
+                    header("Location: ../login.html?signup=success");
                     exit();
                 }
             }
@@ -55,6 +55,6 @@ if (isset($_POST['signup_submit'])) {
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
 } else {
-    header("Location: ../signup.php");
+    header("Location: ../signup.html");
     exit();
 }
