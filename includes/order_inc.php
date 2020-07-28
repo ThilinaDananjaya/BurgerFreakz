e554ba3bac4c4cacca20f1fedb69a26420928d53
Thilina Dananjaya <66713156+ThilinaDananjaya@users.noreply.github.com>
2020-07-29 02:30:54 +0530

    Update order_inc.php

--- includes/order_inc.php
+++ includes/order_inc.php
@@ -10,21 +10,22 @@ if (isset($_POST['order_submit'])) {
     $noOfItems3 = $_POST['noOfItems3'];
     $noOfItems4 = $_POST['noOfItems4'];
     $noOfItems5 = $_POST['noOfItems5'];
+    $noOfItems6 = $_POST['noOfItems6'];
 
     if (empty($name) || empty($telNumber) || empty($address)) {
         header("Location: ../order.php?error=emptyfields");
         exit();
-    } elseif (($noOfItems1 == 0) && ($noOfItems2 == 0) && ($noOfItems3 == 0) && ($noOfItems4 == 0) && ($noOfItems5 == 0)) {
+    } elseif (($noOfItems1 == 0) && ($noOfItems2 == 0) && ($noOfItems3 == 0) && ($noOfItems4 == 0) && ($noOfItems5 == 0) && ($noOfItems6 == 0)) {
         header("Location: ../order.php?error=noorders");
         exit();
     } else {
-        $sql = "INSERT INTO orders (customer_name, tel_no, customer_address, crispy_chicken_cheese_burger, veggie_burger, egg_burger, beef_burger, large_burger) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+        $sql = "INSERT INTO orders (customer_name, tel_no, customer_address, chicken_submarines, large_burger, beef_burger, egg_burger, crispy_burger, veggie_burger) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
             header("Location: ../order.php?error=sqlerror");
             exit();
         } else {
-            mysqli_stmt_bind_param($stmt, "sssiiiii", $name, $telNumber, $address, $noOfItems1, $noOfItems2, $noOfItems3, $noOfItems4, $noOfItems5);
+            mysqli_stmt_bind_param($stmt, "sssiiiiii", $name, $telNumber, $address, $noOfItems1, $noOfItems2, $noOfItems3, $noOfItems4, $noOfItems5, $noOfItems6);
             mysqli_stmt_execute($stmt);
             header("Location: ../index_logged.php?order=success");
             exit();
