4feb22d45f2e26a21ecd8e4dba4af42e08656202
Thilina Dananjaya <66713156+ThilinaDananjaya@users.noreply.github.com>
2020-07-29 12:41:09 +0530

    Update terms_of_service.php

--- terms_of_service.php
+++ terms_of_service.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap - Prebuilt Layout</title>
+    <title>Terms of Service</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="termsstyle.css">
